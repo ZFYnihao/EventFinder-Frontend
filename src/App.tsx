fc26f0e2b72926250329dccff594e15292fa2a07
RyanKuang <ryankuang0618@gmail.com>
2025-02-12 19:40:25 -0800

    codebase for frontend

---
+++ src/App.tsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { GoogleLogin, googleLogout } from '@react-oauth/google'
+import {jwtDecode} from "jwt-decode"
+import './App.css'
+
+function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  interface User {
+    given_name: string;
+    family_name: string;
+    name: string;
+    email: string;
+  }
+
+  return (
+    <>
+        <div>
+        <GoogleLogin 
+          onSuccess={(credentialResponse) => 
+              {
+                const decoded : Partial<User> = jwtDecode(credentialResponse.credential!) as Partial<User>
+                const userData: User = {
+                  given_name: decoded.given_name ?? "",
+                  family_name: decoded.family_name ?? "",
+                  name: decoded.name ?? "",
+                  email: decoded.email ?? "",
+                };
+    
+                console.log(userData);
+                setUser(userData);
+              } 
+            } 
+          onError={() => console.log("login failed")}
+          useOneTap
+        />
+        <button
+            onClick={() => {
+              googleLogout();
+              localStorage.clear();
+              sessionStorage.clear();
+              console.log("test")
+            }}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              background: "white",
+              border: "1px solid #ddd",
+              borderRadius: "5px",
+              padding: "10px 20px",
+              cursor: "pointer",
+              fontSize: "16px",
+            }}
+          >
+            <img
+              src="https://developers.google.com/identity/images/g-logo.png"
+              alt="Google logo"
+              style={{ width: "20px", marginRight: "10px" }}
+            />
+            Logout
+          </button>
+        </div>
+        
+    </>
+  )
+}
+
+export default App
